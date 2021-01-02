3bc1e0a34c6c12335a2ec898072c18faf7a5b0b1
CAI <cai1510120388@gmail.com>
2021-01-02 10:27:03 +0100

    install.php

--- src/Model/install.php
+++ src/Model/install.php
@@ -1,14 +1,30 @@
 <?php // la base de donnees, les tables et insere les donnees 
+//tester et executer
 function Test($link,$requete)
 	{ 
+		//init execute time
+		ini_set("max_execution_time",180000);
+		set_time_limit(0);
+		
+		//test
 		$resultat=mysqli_query($link,$requete) or die("$requete : ".mysqli_error($link));
 		return($resultat);
 	}
-
+	
+//remplacer les symboles special
 function replace_specialChar($strParam){
-	$regex = "/\/|\～|\，|\。|\！|\？|\“|\”|\【|\】|\『|\』|\：|\；|\《|\》|\’|\‘|\ |\·|\~|\!|\@|\#|\\$|\%|\^|\&|\*|\(|\)|\_|\+|\{|\}|\:|\<|\>|\?|\[|\]|\,|\.|\/|\;|\'|\`|\-|\=|\\\|\|/";
-    return preg_replace($regex,"",$strParam);
-}	
+	if(check_str($strParam)){
+		$regex = "/\/|\～|\，|\。|\！|\？|\“|\”|\【|\】|\『|\』|\：|\；|\《|\》|\’|\‘|\ |\·|\~|\!|\@|\#|\\$|\%|\^|\&|\*|\(|\)|\_|\+|\{|\}|\:|\<|\>|\?|\[|\]|\,|\.|\/|\;|\'|\`|\-|\=|\\\|\|/";
+		return preg_replace($regex,"",$strParam);
+	}
+	return $strParam;
+}
+	
+//tester si il y a les symboles special
+function check_str($str){
+	$res = preg_match("/\/|\～|\，|\。|\！|\？|\“|\”|\【|\】|\『|\』|\：|\；|\《|\》|\’|\‘|\ |\·|\~|\!|\@|\#|\\$|\%|\^|\&|\*|\(|\)|\_|\+|\{|\}|\:|\<|\>|\?|\[|\]|\,|\.|\/|\;|\'|\`|\-|\=|\\\|\|/", $str);
+	return $res ? TRUE : FALSE;
+}
 	
 class SqlDB{
 
@@ -25,24 +41,11 @@ class SqlDB{
 		$Sql="
 				DROP DATABASE IF EXISTS $base;
 				CREATE DATABASE $base;
-				USE $base;
-				CREATE TABLE region (id INT AUTO_INCREMENT PRIMARY KEY, lib VARCHAR(255) NOT NULL);
-				CREATE TABLE departement (id INT AUTO_INCREMENT PRIMARY KEY, region INT NOT NULL, lib VARCHAR(255) NOT NULL);
-				
-				INSERT INTO region VALUES (1, 'Lorraine');
-				INSERT INTO region VALUES (2, 'Alsace');
-				
-				INSERT INTO departement VALUES (1, 1, 'Moselle');
-				INSERT INTO departement VALUES (2, 1, 'Meurthe-et-Moselle');
-				INSERT INTO departement VALUES (3, 1, 'Vosges');
-				INSERT INTO departement VALUES (4, 1, 'Meuse');
-				
-				INSERT INTO departement VALUES (5, 2, 'Bas-Rhin');
-				INSERT INTO departement VALUES (6, 2, 'Haut-Rhin');";
+				USE $base;";
 
 		include 'Donnees.inc.php';
 		
-		
+		//create table
 		$Sql .= 'CREATE TABLE sous_categorie (nom VARCHAR(255) NOT NULL,sous VARCHAR(255) NOT NULL);' ;
 		$Sql .= 'CREATE TABLE super_categorie (nom VARCHAR(255) NOT NULL,super VARCHAR(255) NOT NULL);' ;
 		
@@ -61,17 +64,16 @@ class SqlDB{
 					if ( get_magic_quotes_gpc ()){  // s'il n'y a pas de "'" dedans
 					   $Content1 = stripslashes ( $Content2 ); 
 					}
+					
+					//inserer table
 					$Sql .= "INSERT INTO sous_categorie VALUES ('$Content1','$Content2');";
 				}
 			}
 			
-	/*		//s'il existe super-categorie dans ce array
+			//s'il existe super-categorie dans ce array
 			if(array_key_exists('super-categorie', $contenu_categorie)){
 				foreach($contenu_categorie['super-categorie'] as $numero=>$super_categorie)
 				{
-
-					
-				
 					// supprimer " ' " commme " Partie d'orange " pour eviter bug
 					$Content1 = addslashes($categorie);
 					if ( get_magic_quotes_gpc ()){  // s'il n'y a pas de "'" dedans
@@ -81,58 +83,48 @@ class SqlDB{
 					if ( get_magic_quotes_gpc ()){  // s'il n'y a pas de "'" dedans
 					   $Content1 = stripslashes ( $Content2 ); 
 					}
+					
+					//inserer table
 					$Sql .= "INSERT INTO super_categorie VALUES ('$Content1','$Content2');";
 				}
-			}*/
+			}
 		}	
-	/*
-
+	
+		//create table
 		$Sql .= 'CREATE TABLE Recettes (num_recette INT NOT NULL,titre VARCHAR(255) NOT NULL,ingredients VARCHAR(255) NOT NULL,preparation TEXT ,pre_index INT NOT NULL,sous_recettes  VARCHAR(255) NOT NULL );' ;
+		$Sql .= 'CREATE TABLE Prefers (num_recette INT NOT NULL,titre VARCHAR(255) NOT NULL,num_prefers INT);' ;
+		
 		//array Recettes
 		foreach($Recettes as $index=>$contenu_recettes){
 			foreach($contenu_recettes['index'] as $pre_index=>$sous_recettes)
 			{
-				//echo $contenu_recettes['titre'].'<br>';
-				//echo $contenu_recettes['ingredients'].'<br>';
-				//echo $contenu_recettes['preparation'].'<br>';
-				//echo $pre_index.'<br>';
-				//echo $sous_recettes.'<br>';
-				
 				// supprimer " ' " commme " Partie d'orange " pour eviter bug
 				$titre =mysqli_real_escape_string($mysqli,$contenu_recettes['titre']);
-
 				$ingredients=replace_specialChar($contenu_recettes['ingredients']);
-
 				$preparation=replace_specialChar($contenu_recettes['preparation']);
-
 				$sous_recettes = addslashes($sous_recettes);
 				if ( get_magic_quotes_gpc ()){  // s'il n'y a pas de "'" dedans
 					  $$sous_recettes = stripslashes ( $sous_recettes ); 
 				}
-				$Sql .= "INSERT INTO Recettes VALUES ('$index','$titre','$ingredients','$preparation','$pre_index','$sous_recettes');";
-			}
-			
-		}	*/
 				
+				//inserer table
+				$Sql .= "INSERT INTO Recettes VALUES ('$index','$titre','$ingredients','$preparation','$pre_index','$sous_recettes');";
+				$Sql .= "INSERT INTO Prefers (num_recette,titre) VALUES ('$index','$titre');";
+			}
+		}	
+		
+		//supprimer denriere ; pour eviter query null
 		$Sql = substr($Sql,0,strlen($Sql)-1); 
 				
-		$query = "SELECT * FROM region";
-				
+		//test
 		foreach(explode(';',$Sql) as $Requete) Test($mysqli,$Requete);
 
-		$resultat = $mysqli->query($query);
-		
 		$query2 = "SELECT * FROM sous_categorie";
 		
 		$resultat2 = $mysqli->query($query2);
 
 		mysqli_close($mysqli);
 
-		while($nuplet = $resultat->fetch_assoc())
-			{ 
-				echo "\t".$nuplet["lib"]."\n";
-			}
-			
 		while($nuplet2 = $resultat2->fetch_assoc())
 			{ 
 				echo "\t".$nuplet2["nom"]."\n";
@@ -145,23 +137,21 @@ class SqlDB{
 	{ 
 		//connexion de sql
 		$mysqli=mysqli_connect('127.0.0.1', 'root', '') or die("Erreur de connexion");
-		$base="Regions";
+		$base="boisson_db";
 		$Sql="
 			USE $base";
 		
 		foreach(explode(';',$Sql) as $Requete) Test($mysqli,$Requete);		
 		
-		$query = "SELECT * FROM departement";
+		$query = "SELECT * FROM sous_categorie";
 				
-
-
 		$resultat = $mysqli->query($query);
 
 		mysqli_close($mysqli);
 		
 		while($nuplet = $resultat->fetch_assoc())
 			{ 
-				echo "\t".$nuplet["lib"]."\n";
+				echo "\t".$nuplet["nom"]."\n";
 			}
 	}
 }
